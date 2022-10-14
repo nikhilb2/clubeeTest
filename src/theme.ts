5d4a2773c3a4d3f6edfd3a731143743752d3ba6f
Nikhil Bhatia <nikhilbhatia2004@gmail.com>
2022-10-14 13:50:53 +0200

    initial setup

---
+++ src/theme.ts
@@ -0,0 +1,128 @@
+import { createStyles, createTheme } from "@mui/material/styles"
+import { Shadows } from "@mui/material/styles/shadows"
+
+export const fonts = {
+  primary: {},
+  secondary: {
+    regular: "Mukta",
+  },
+  tertiary: {
+    regular: "Roboto",
+  },
+}
+
+export const colors = {
+  gray: "#EEEEEE",
+  black: "#1B1B1B",
+  black2: "#000A12",
+  gray7: "#F3F3F3",
+  gray1: "#A3A3A3",
+  gray2: "#E1E1E1",
+  hoverBlue: "#F2F8FF",
+  gray3: "#828282",
+  gray4: "#757474",
+  gray5: "#BCBBBB",
+  gray6: "#DDDBDB",
+  divider: "#E1E1E1",
+  danger: "#FF8F8F",
+}
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: 20,
+    },
+    body1: {},
+  },
+  palette: {
+    primary: {
+      main: "#382933",
+      contrastText: "#a4b494",
+      //      light: "#F2F8FF",
+    },
+    secondary: {
+      main: "#a4b494",
+      contrastText: "#a4b494",
+    },
+    background: {
+      paper: "#F2F8FF",
+    },
+    text: {
+      primary: "rgba(0,0,0,.9)",
+      secondary: "#9597A6",
+    },
+    action: {
+      hover: "#66CEF5",
+      disabledBackground: "#E1E1E1",
+      disabled: "#ffffff",
+    },
+    error: {
+      main: "#E90101",
+    },
+    success: {
+      main: "#4BB543",
+    },
+    info: {
+      main: "#66CEF5",
+      dark: "#2294C1",
+      contrastText: "#ffffff",
+    },
+  },
+  spacing: [0, 4, 8, 16, 32, 64, 96, 156],
+  shadows: new Array(25).fill("none") as Shadows,
+})
+
+export const moreStyles = createStyles({
+  showScroll: {
+    "::-webkit-scrollbar": {
+      WebkitAppearance: "none",
+      width: "7px",
+      height: "7px",
+    },
+
+    "::-webkit-scrollbar-thumb": {
+      borderRadius: "4px",
+      backgroundColor: "rgba(0, 0, 0, .5)",
+      boxShadow: "0 0 1px rgba(255, 255, 255, .5)",
+    },
+  },
+  showScrollX: {
+    "::-webkit-scrollbar": {
+      WebkitAppearance: "none",
+      width: "0px",
+      height: "7px",
+    },
+
+    "::-webkit-scrollbar-thumb": {
+      borderRadius: "4px",
+      backgroundColor: "rgba(0, 0, 0, .5)",
+      boxShadow: "0 0 1px rgba(255, 255, 255, .5)",
+    },
+  },
+  showScrollY: {
+    "::-webkit-scrollbar": {
+      WebkitAppearance: "none",
+      width: "7px",
+      height: "0px",
+    },
+
+    "::-webkit-scrollbar-thumb": {
+      borderRadius: "4px",
+      backgroundColor: "rgba(0, 0, 0, .5)",
+      boxShadow: "0 0 1px rgba(255, 255, 255, .5)",
+    },
+  },
+  link: {
+    fontWeight: 600,
+    color: theme.palette.primary.main,
+    textDecoration: "underline",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  shadow: {
+    boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+  },
+})
+
+export default theme
