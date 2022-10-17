d556cd3e2f2576782c3307036b6f97db8bad4d0b
Nikhil Bhatia <nikhilbhatia2004@gmail.com>
2022-10-17 23:30:01 +0200

    create news form

--- src/components/common/createNewsButton.tsx
+++ src/components/common/createNewsButton.tsx
@@ -1,26 +1,150 @@
-import { Fab, Box } from "@mui/material"
-import React from "react"
+import {
+  Fab,
+  Box,
+  Typography,
+  Stack,
+  FormControlLabel,
+  TextField,
+  Button,
+  Collapse,
+} from "@mui/material"
+import React, { useCallback, useEffect, useMemo, useState } from "react"
 import AddIcon from "@mui/icons-material/Add"
+import SimpleModal from "../modals/simpleModal"
+import Image from "next/image"
 const CreateNews = () => {
+  const [image, setImage] = useState<number | undefined>()
+  const [imageError, setImageError] = useState("")
+  const [showCreateNews, setShowCreateNews] = useState(false)
+  const [title, setTitle] = useState("")
+  const [titleError, setTitleError] = useState("")
+  const [description, setDescription] = useState("")
+  const [descriptionError, setDescriptionError] = useState("")
+
+  const validateForm = useCallback(() => {
+    if (title.length < 5) {
+      setTitleError("Title should be atleast 5 characters long")
+    }
+    if (description.length < 200) {
+      setDescriptionError("Description should be atleast 200 characters")
+    }
+
+    if (!image) {
+      setImageError("Please select an image")
+    }
+  }, [image, title, description])
+
+  useEffect(() => {
+    if (title.length >= 5) {
+      setTitleError("")
+    }
+    if (description.length >= 200) {
+      setDescriptionError("")
+    }
+
+    if (image) {
+      setImageError("")
+    }
+  }, [image, title, description])
+
   return (
-    <Box
-      sx={{
-        position: "fixed",
-        top: {
-          xs: undefined,
-          md: 20,
-        },
-        bottom: {
-          xs: 20,
-          md: undefined,
-        },
-        right: 20,
-      }}
-    >
-      <Fab>
-        <AddIcon />
-      </Fab>
-    </Box>
+    <>
+      <Box
+        sx={{
+          position: "fixed",
+          top: {
+            xs: undefined,
+            md: 20,
+          },
+          bottom: {
+            xs: 20,
+            md: undefined,
+          },
+          right: 20,
+        }}
+      >
+        <Fab onClick={() => setShowCreateNews(true)}>
+          <AddIcon />
+        </Fab>
+      </Box>
+      <SimpleModal
+        open={showCreateNews}
+        onClose={() => setShowCreateNews(false)}
+      >
+        <Stack width={700}>
+          <Typography variant="h5">Create news</Typography>
+          <Typography variant="caption">Fill in the details below.</Typography>
+          <Stack mt={2} spacing={3}>
+            <Stack>
+              <Typography>Title</Typography>
+              <TextField
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Enter the news title here."
+              />
+              <Collapse in={!!titleError}>
+                <Typography color="error" variant="caption">
+                  {titleError}
+                </Typography>
+              </Collapse>
+            </Stack>
+            <Stack>
+              <Typography>Description</Typography>
+              <TextField
+                onChange={(e) => setDescription(e.target.value)}
+                maxRows={5}
+                multiline={true}
+                placeholder="Enter a long description here."
+              />
+              <Collapse in={!!descriptionError}>
+                <Typography color="error" variant="caption">
+                  {descriptionError}
+                </Typography>
+              </Collapse>
+            </Stack>
+            <Stack direction="row" alignItems={"center"} spacing={2}>
+              <Button
+                variant="contained"
+                onClick={() => setImage(Math.random())}
+              >
+                Click here
+              </Button>
+              <Typography>to select random image</Typography>
+              <Collapse in={!!titleError}>
+                <Typography color="error" variant="caption">
+                  {imageError}
+                </Typography>
+              </Collapse>
+            </Stack>
+            <Collapse in={!!image}>
+              <Stack height="200px" width="300px">
+                {!!image && (
+                  <Image
+                    src={`https://loremflickr.com/1000/500/sports?random=${image}`}
+                    width="300px"
+                    height="200px"
+                    priority
+                    objectFit="cover"
+                  />
+                )}
+              </Stack>
+            </Collapse>
+          </Stack>
+          <Stack direction="row" spacing={2} justifyContent="center" mt={3}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                validateForm()
+              }}
+            >
+              Accept
+            </Button>
+            <Button variant="outlined" onClick={() => setShowCreateNews(false)}>
+              Cancel
+            </Button>
+          </Stack>
+        </Stack>
+      </SimpleModal>
+    </>
   )
 }
 
---
+++ src/components/modals/simpleModal.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import Box from "@mui/material/Box"
+
+import Modal from "@mui/material/Modal"
+import { IconButton, Stack } from "@mui/material"
+import CloseIcon from "@mui/icons-material/Close"
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+}
+
+interface SimpleModalProps {
+  open: boolean
+  onClose(): void
+  children: React.ReactNode
+}
+export default function SimpleModal(props: SimpleModalProps) {
+  const { open, onClose, children } = props
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Box sx={style}>
+        {children}
+        <Box position="absolute" top={3} right={3}>
+          <IconButton onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </Modal>
+  )
+}
