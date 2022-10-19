1d74a42a23ac52feaf60f4d929e10dd64454c840
Nikhil Bhatia <nikhilbhatia2004@gmail.com>
2022-10-19 08:22:45 +0200

    success animation

--- src/components/common/createNewsButton.tsx
+++ src/components/common/createNewsButton.tsx
@@ -1,5 +1,5 @@
 import { Fab, Box, Typography, Stack, Button, Collapse } from "@mui/material"
-import React, { useCallback, useEffect, useMemo, useState } from "react"
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import AddIcon from "@mui/icons-material/Add"
 import SimpleModal from "../modals/simpleModal"
 import Image from "next/image"
@@ -7,6 +7,8 @@ import SimpleInput from "../inputs/simpleInput"
 import { useMutation } from "react-query"
 import { PostNewsParams } from "src/model"
 import { postNews } from "src/queries/news"
+import Lottie, { LottieRef, LottieRefCurrentProps } from "lottie-react"
+import * as animation from "src/lottie/successful.json"
 
 interface CreateNewsProps {
   userName: string
@@ -64,9 +66,31 @@ const CreateNews = (props: CreateNewsProps) => {
     }
   }, [image, title, description, author])
 
-  const { mutate: mutatePostNews } = useMutation((params: PostNewsParams) => {
-    return postNews(params)
-  })
+  const {
+    mutate: mutatePostNews,
+    isSuccess,
+    reset,
+  } = useMutation(
+    (params: PostNewsParams) => {
+      return postNews(params)
+    },
+    {
+      onSuccess: () => {
+        setTimeout(() => {
+          animationRef.current?.play()
+        }, 500)
+      },
+    }
+  )
+
+  const resetForm = useCallback(() => {
+    setTitle("")
+    setDescription("")
+    setImage(undefined)
+    reset()
+  }, [reset])
+
+  const animationRef = useRef<LottieRefCurrentProps>(null)
 
   return (
     <>
@@ -90,83 +114,117 @@ const CreateNews = (props: CreateNewsProps) => {
       </Box>
       <SimpleModal
         open={showCreateNews}
-        onClose={() => setShowCreateNews(false)}
+        onClose={() => {
+          setShowCreateNews(false)
+          resetForm()
+        }}
       >
         <Stack width={700}>
-          <Typography variant="h5">Create news</Typography>
-          <Typography variant="caption">Fill in the details below.</Typography>
-          <Stack mt={2} spacing={2}>
-            <SimpleInput
-              label="Title"
-              value={title}
-              onChange={(text) => setTitle(text)}
-              placeholder="Enter the news title here."
-              error={titleError}
-            />
+          <Collapse in={!isSuccess}>
+            <Typography variant="h5">Create news</Typography>
+            <Typography variant="caption">
+              Fill in the details below.
+            </Typography>
+            <Stack mt={2} spacing={2}>
+              <SimpleInput
+                label="Title"
+                value={title}
+                onChange={(text) => setTitle(text)}
+                placeholder="Enter the news title here."
+                error={titleError}
+              />
 
-            <SimpleInput
-              label="Author name"
-              error={authorError}
-              value={author}
-              onChange={(text) => setAuthor(text)}
-            />
+              <SimpleInput
+                label="Author name"
+                error={authorError}
+                value={author}
+                onChange={(text) => setAuthor(text)}
+              />
 
-            <SimpleInput
-              label="Description"
-              error={descriptionError}
-              onChange={(text) => setDescription(text)}
-              multiline={true}
-              placeholder="Enter a long description here."
-            />
+              <SimpleInput
+                label="Description"
+                error={descriptionError}
+                onChange={(text) => setDescription(text)}
+                multiline={true}
+                placeholder="Enter a long description here."
+              />
 
-            <Stack direction="row" alignItems={"center"} spacing={2}>
-              <Button
-                variant="contained"
-                onClick={() => setImage(Math.random())}
-              >
-                Click here
-              </Button>
-              <Typography>to select random image</Typography>
-              <Collapse in={!!imageError}>
-                <Typography color="error" variant="caption">
-                  {imageError}
-                </Typography>
+              <Stack direction="row" alignItems={"center"} spacing={2}>
+                <Button
+                  variant="contained"
+                  onClick={() => setImage(Math.random())}
+                >
+                  Click here
+                </Button>
+                <Typography>to select random image</Typography>
+                <Collapse in={!!imageError}>
+                  <Typography color="error" variant="caption">
+                    {imageError}
+                  </Typography>
+                </Collapse>
+              </Stack>
+              <Collapse in={!!image}>
+                <Stack height="200px" width="300px">
+                  {!!image && (
+                    <Image
+                      src={`https://loremflickr.com/1000/500/sports?random=${image}`}
+                      width="300px"
+                      height="200px"
+                      priority
+                      objectFit="cover"
+                    />
+                  )}
+                </Stack>
               </Collapse>
             </Stack>
-            <Collapse in={!!image}>
-              <Stack height="200px" width="300px">
-                {!!image && (
-                  <Image
-                    src={`https://loremflickr.com/1000/500/sports?random=${image}`}
-                    width="300px"
-                    height="200px"
-                    priority
-                    objectFit="cover"
-                  />
-                )}
+            <Stack direction="row" spacing={2} justifyContent="center" mt={3}>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  if (validateForm()) {
+                    mutatePostNews({
+                      title,
+                      description,
+                      image: `https://loremflickr.com/1000/500/sports?random=${image}`,
+                      author,
+                    })
+                  }
+                }}
+              >
+                Accept
+              </Button>
+              <Button
+                variant="outlined"
+                onClick={() => setShowCreateNews(false)}
+              >
+                Cancel
+              </Button>
+            </Stack>
+          </Collapse>
+          <Collapse in={isSuccess}>
+            <Stack alignItems={"center"} spacing={3}>
+              <Stack width={200}>
+                <Lottie
+                  lottieRef={animationRef}
+                  animationData={animation}
+                  loop={false}
+                  autoplay={false}
+                />
               </Stack>
-            </Collapse>
-          </Stack>
-          <Stack direction="row" spacing={2} justifyContent="center" mt={3}>
-            <Button
-              variant="contained"
-              onClick={() => {
-                if (validateForm()) {
-                  mutatePostNews({
-                    title,
-                    description,
-                    image: `https://loremflickr.com/1000/500/sports?random=${image}`,
-                    author,
-                  })
-                }
-              }}
-            >
-              Accept
-            </Button>
-            <Button variant="outlined" onClick={() => setShowCreateNews(false)}>
-              Cancel
-            </Button>
-          </Stack>
+              <Typography>Hurray! News has been added.</Typography>
+              <Button
+                onClick={() => {
+                  resetForm()
+                  setShowCreateNews(false)
+                }}
+                sx={{
+                  width: "fit-content",
+                }}
+              >
+                Close
+              </Button>
+            </Stack>
+          </Collapse>
         </Stack>
       </SimpleModal>
     </>
