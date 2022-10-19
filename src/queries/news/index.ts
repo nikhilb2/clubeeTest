b1e717f5d2ab19dcc448f6666404b1791c3667d5
Nikhil Bhatia <nikhilbhatia2004@gmail.com>
2022-10-19 13:13:13 +0200

    log error

--- src/queries/news/index.ts
+++ src/queries/news/index.ts
@@ -17,6 +17,7 @@ export const getNews = async () => {
   if (result.success) {
     return result.data as News[]
   }
+  console.log(result)
 
   throw new Error(result.error, {
     cause: new Error(result.message),
